5fe2795c8a8b6d8d444419b313ee5ccde666dcb9
Pronoy Das <pronoysunny.47@gmail.com>
2025-01-15 20:29:43 +0530

    Code Push

---
+++ src/test/java/com/example/jenkinsdemo/LoginTest.java
@@ -0,0 +1,23 @@
+package com.example.jenkinsdemo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class LoginTest {
+
+
+    @Test
+    public void setUp() throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+        Thread.sleep(5000);
+        driver.close();
+
+    }
+
+}
